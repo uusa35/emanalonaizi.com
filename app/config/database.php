1950c4b69582d6679121c35a40c49970fdd3835f
root <root@khetbah.com>
2014-09-29 12:54:52 -0400

    adding and commiting from server

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'eman',
+			'username'  => 'kaizen',
+			'password'  => 'kaizen_db_user',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
