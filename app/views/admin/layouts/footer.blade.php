f630fce658f407869c0dac0abbdf430c672d47b3
Usama Ahmed <uusa35@gmail.com>
2014-10-02 22:20:58 +0300

    deleted some templates

--- app/views/admin/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-            </div>
-        </div>
-    </div>
-</footer>
--- app/views/site/partials/home.categories.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="col-md-1 col-sm-1"></div>
-<div class="col-md-10 col-sm-10">
-    {{ HTML::image('images/bakground.png', 'background',array('class'=> 'img-responsive')) }}
-</div>
-<div class="col-md-1 col-sm-1"></div>
