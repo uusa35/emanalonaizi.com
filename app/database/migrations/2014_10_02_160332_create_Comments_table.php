9ac77cc0b02639426247ed984e3fc1b0bcbb131f
Usama Ahmed <uusa35@gmail.com>
2014-10-26 20:55:01 +0300

    another changes

--- app/database/migrations/2014_10_02_160332_create_Comments_table.php
+++ app/database/migrations/2014_10_02_160332_create_Comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCommentsTable extends Migration {
+class CreatecommentsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCommentsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('Comments', function(Blueprint $table)
+		Schema::create('comments', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('title');
