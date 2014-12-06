c087e12e07c95d524692b626f43a7ea375fdbefd
Usama Ahmed <uusa35@gmail.com>
2014-12-06 17:50:50 +0300

    fix small bug in the category.index view

--- app/views/site/category/index.blade.php
+++ app/views/site/category/index.blade.php
@@ -1,23 +1,26 @@
 @extends('site.layouts._one_col')
 
 @section('content')
-<div class="row">
+
 
     @if($category->category_description)
-    <div class="col-md-10 col-md-offset-1 ">
-        <div class="alert alert-info text-center text-success " >
-            <h4 style="line-height: 161%;"><i class="fa fa-fw fa-pencil"></i><strong> {{ $category->category_description }}</strong></h4>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 ">
+                <div class="alert alert-info text-center text-success " >
+                    <h4 style="line-height: 161%;"><i class="fa fa-fw fa-pencil"></i><strong> {{ $category->category_description }}</strong></h4>
+                </div>
+            </div>
         </div>
-    </div>
     @endif
 
-</div>
+
 
 @foreach ($posts as $post)
 
     @include('site.posts.post_template')
 
 @endforeach
+
 {{ $posts->links() }}
 
 @stop
