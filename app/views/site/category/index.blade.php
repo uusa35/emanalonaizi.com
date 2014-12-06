285babc3b87001720b984540771d6a5372ac3a3c
Usama Ahmed <uusa35@gmail.com>
2014-12-06 16:52:54 +0300

    fix one small bug in the categoery.index view

--- app/views/site/category/index.blade.php
+++ app/views/site/category/index.blade.php
@@ -16,8 +16,7 @@
 @foreach ($posts as $post)
 
         <!-- Post Title -->
-        <div class="panel">
-            <div class="panel-body">
+
                 <div class="row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8" style="padding: 20px; border:1px solid white ; margin-bottom: 20px; background-color: #ececec !important;overflow: hidden !important;">
@@ -57,10 +56,10 @@
                 </div><!-- end coloum-m-9-->
                 <div class="col-md-2"></div>
         </div> <!-- end row -->
-    </div></div> <!-- end of panel and panel body -->
+
 @endforeach
 {{ $posts->links() }}
-</div>
+</div> <!-- end row before foreach --!>
 @stop
 
 
